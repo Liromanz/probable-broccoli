9abd0c9282048d5a152c0226206ceaefa105c330
Liromanz <kiss_wiwa_2003@mail.ru>
2023-09-14 11:04:04 +0300

    первый раз отправляю

---
+++ consolka/Program.cs
@@ -0,0 +1,32 @@
+﻿string txt;
+do
+{
+    Console.WriteLine("Выберите цвет");
+    Console.WriteLine("1. Красный");
+    Console.WriteLine("2. Коричневый");
+    Console.WriteLine("3. Черный");
+    Console.WriteLine("4. Желтый");
+    Console.WriteLine("5. никакой");
+    txt = Console.ReadLine();
+    /* if (txt == "1")
+     {
+         int a = 5;
+         Console.WriteLine(a);
+     }
+     else if (txt == "2")
+     {
+         Console.WriteLine("коричневый классный");
+     }*/
+
+    switch (txt)
+    {
+        case "1":
+            Console.WriteLine("sdfsd");
+            break;
+        case "2":
+            Console.WriteLine("коричневыйыывалывдаоыдвлоа");
+            break;
+        case "3":
+            break;
+    }
+} while (txt != "5");
